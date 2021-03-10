9170f910c3fcd965c6634d253798a450eb347b84
jwrightazure <43790525+jwrightazure@users.noreply.github.com>
2021-03-10 13:23:03 -0500

    Delete test.sh

--- generate-null-static-routes-script/test.sh
+++
@@ -1,2 +0,0 @@
-az network route-table route create --resource-group AZFW --name route0 --route-table-name AZFW1-RT --address-prefix 10.0.0.0/24 --next-hop-type VirtualAppliance --next-hop-ip-address 10.20.0.4
-az network route-table route create --resource-group AZFW --name route1 --route-table-name AZFW1-RT --address-prefix 10.0.1.0/24 --next-hop-type VirtualAppliance --next-hop-ip-address 10.20.0.4
