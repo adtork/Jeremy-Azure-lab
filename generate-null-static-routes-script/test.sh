c8b4e50cb01f4cda9a4d668ea8a5f72e49110d1e
jwrightazure <43790525+jwrightazure@users.noreply.github.com>
2021-03-10 13:34:05 -0500

    Delete test.sh

--- generate-null-static-routes-script/test.sh
+++
@@ -1,3 +0,0 @@
-az group create --name udr-test --location westus2
-az network route-table create --name RT --resource-group udr-test --location westus2
-az network route-table route create --resource-group udr-test --name route0 --route-table-name RT --address-prefix 10.0.0.0/24 --next-hop-type VirtualAppliance --next-hop-ip-address 10.20.0.4
